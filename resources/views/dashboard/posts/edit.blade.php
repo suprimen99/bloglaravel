987c2d7cbb0e4d2da6eb8619a41b7d5bf7eb9ad5
suprimen99 <suprianto6599@gmail.com>
2022-12-26 20:20:33 +0700

    update3

---
+++ resources/views/dashboard/posts/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 p-2 mb-2 border-bottom">
+    <h1 class="h2"> Edit Post</h1>
+</div>
+
+<div class="row">
+    <div class="col-md-8">
+        <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+            @method('put')
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label @error('title') is-invalid @enderror">Title</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
+                @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="slug" class="form-label @error('slug') is-invalid @enderror">Slug</label>
+                <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug', $post->slug) }}">
+                @error('slug')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="category" class="form-label">Category</label>
+                <select class="form-select" name="category_id">
+                    @foreach ($categories as $category )
+                    @if(old('category_id', $post->category_id) == $category->id)
+                    <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                    @else
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label">Body</label>
+                @error('body')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+                <input id="body" type="hidden" name="body" value="{{ old('body', $post->body) }}">
+                <trix-editor input="body"></trix-editor>
+            </div>
+            <button type="submit" class="btn btn-primary">Update Post</button>
+        </form>
+
+    </div>
+</div>
+
+<script>
+const title = document.querySelector('#title');
+const slug = document.querySelector('#slug');
+
+
+title.addEventListener('change', function() {
+    fetch('/dashboard/posts/checkSlug?title=' + title.value)
+        .then(response => response.json())
+        .then(data => slug.value = data.slug)
+});
+
+document.addEventListener('trix-file-accept', function(e) {
+    e.prevenDefault();
+})
+</script>
+@endsection
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,14 +6,20 @@
     <div class="col-lg-8">
         <h1 class="mb-3">{{ $post->title }}</h1>
         <a href="/dashboard/posts" class="btn btn-success"> <span data-feather="arrow-left"></span> Back to my posts</a>
-        <a href="" class="btn btn-warning"> <span data-feather="edit"></span> Edit</a>
-        <a href="" class="btn btn-danger"> <span data-feather="x-circle"></span> Delete</a>
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"> <span data-feather="edit"></span> Edit</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger" onclick="return confirm('Are you Sure')">
+                    <span data-feather="x-circle"></span> Delete
+                </button>
+            </form>
 <img src="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid my-3 ">
         <article my-3 fs-4>
 {!! $post->body !!}
         </article>
 
-<br>
+
     </div>
 </div>
 </div>
